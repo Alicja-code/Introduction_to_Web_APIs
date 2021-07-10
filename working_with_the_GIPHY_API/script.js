257fa4ba0a358ea404ab083e986fc8c72d6d90b5
Alicja <32157812+AlicjaHelenaM@users.noreply.github.com>
2021-07-10 21:20:07 +0200

    filter response with parameters

--- working_with_the_GIPHY_API/script.js
+++ working_with_the_GIPHY_API/script.js
@@ -1,6 +1,6 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=WiijRnJiLpbaWvOffwYYnvSR6Ji1G5Qg');
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=WiijRnJiLpbaWvOffwYYnvSR6Ji1G5Qg&tag=squirrels&rating=g');
 
 request.onload = function() {
 	var response = request.response;
